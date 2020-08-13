f9f459cd8a9637db9432c642120de825b784ab45
Lee, Jun Seok <lego3410@gmail.com>
2020-08-13 22:30:08 +0900

    Add tower.py

---
+++ app/tower.py
@@ -0,0 +1,38 @@
+import os
+import serial
+
+class tower:
+    def __init__(self):
+        self.connect()
+
+    def connect(self,
+            stepper1=os.environ["STEPPER1_DEV"],
+            stepper2=os.environ["STEPPER2_DEV"],
+            heater=os.environ["HEATER_DEV"]
+        ):
+        self.stepper1 = serial.Serial(
+            port=stepper1,
+            baudrate=115200
+        )
+
+        self.stepper2 = serial.Serial(
+            port=stepper2,
+            baudrate=115200
+        )
+
+        self.heater = serial.Serial(
+            port=heater,
+            baudrate=19200
+        )
+
+        # micrometer = serial.Serial(
+        #     port=os.environ["MICROMETER_DEV"],
+        #     baudrate=os.environ["MICROMETER_BAUD"]
+        # )
+
+        return self.stepper1, self.stepper2, self.heater
+
+    def disconnect(self):
+        self.stepper1.close()
+        self.stepper2.close()
+        self.heater.close()
