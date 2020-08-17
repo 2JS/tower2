703f074da1447771d1677cb559563ad3a3b04de6
Lee, Jun Seok <lego3410@gmail.com>
2020-08-18 01:22:10 +0900

    Add /dashboard page

--- app/app.py
+++ app/app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import *
 # import tower
 
+pathPrefix = ""
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# if (pathPrefix):
+#     @app.route('/')
+#     def redirect_root():
+#         return redirect(url_for('root'))
+
+@app.route(pathPrefix + '/')
+def index():
+    return render_template('index.html')
+
+@app.route(pathPrefix + '/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
