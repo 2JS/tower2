efc04f23366289564743cf2bee8150d9056dbb5c
Lee, Jun Seok <lego3410@gmail.com>
2020-08-22 02:23:38 +0900

    Implement inc/dec function

---
+++ app/api.py
@@ -0,0 +1,46 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+import tower
+
+tower = tower.Tower()
+
+class Heater(Resource):
+  def post(self):
+    parser = reqparse.RequestParser()
+    parser.add_argument('value', type=float)
+    args = parser.parse_args()
+    print(args)
+    temp = args['value']
+    
+    tower.heater.setTemperature(float(temp))
+    
+    return {}, 200
+  
+  def get(self):
+    return {"value": tower.heater.getTemperature()}, 200
+
+class Extruder(Resource):
+  def post(self):
+    parser = reqparse.RequestParser()
+    parser.add_argument('value')
+    args = parser.parse_args()
+
+    speed = args['value']
+    tower.extruder.setSpeed(float(speed))
+    return {}, 200
+
+  def get(self):
+    return {"value": tower.extruder.getSpeed()}, 200
+
+class Fiber(Resource):
+  def post(self):
+    parser = reqparse.RequestParser()
+    parser.add_argument('value')
+    args = parser.parse_args()
+
+    speed = args['value']
+    tower.fiber.setSpeed(float(speed))
+    return {}, 200
+  
+  def get(self):
+    return {"value": tower.fiber.getSpeed()}, 200
---
+++ app/static/api.js
@@ -0,0 +1,53 @@
+function update() {
+    get('heater');
+    get('extruder');
+    get('fiber')
+}
+
+function get(device) {
+    const req = new Request('/api/' + device, {method:'GET'});
+    fetch(req)
+        .then(response => {
+            if (response.status === 200) {
+                return response.json()
+            }
+        }).then(response => {
+            value = response['value']
+            if (device == 'heater') {
+                heaterinput.value = value;
+            } else if (device == 'extruder') {
+                extruderinput.value = value;
+            } else if (device == 'fiber') {
+                fiberinput.value = value;
+            }
+        }).catch(error => {
+            console.error(error);
+        })
+}
+
+// function getheater() {
+//     return 
+// }
+
+function set(device) {
+    value = document.getElementById(device + '-input').value;
+    var formData = new FormData();
+    formData.append('value', value);
+    const req = new Request('/api/' + device, {method: 'POST', body: formData});
+    fetch(req)
+        .catch(error => console.error(error))
+}
+
+function inc(device, inc=true) {
+    // increase value of given device
+    input = document.getElementById(device + '-input')
+    input.value = parseFloat(input.value) + (inc ? 1 : -1);
+    set(device);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    heaterinput = document.getElementById('heater-input');
+    extruderinput = document.getElementById('extruder-input');
+    fiberinput = document.getElementById('fiber-input');
+    update();
+})
